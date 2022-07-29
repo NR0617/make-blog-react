648a4c45937294e68f48503b6f593e674e11132a
NR0617 <nara20210617@gmail.com>
2022-07-29 12:52:59 +0900

    0729-before-making-usefetch

--- src/blogComponent/CreateBlog.js
+++ src/blogComponent/CreateBlog.js
@@ -1,49 +1,68 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Footer from "../component/Footer";
 
-const CreateBlog = () => {
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('김코딩');
+const CreateBlog = ({ setIsPending }) => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("김코딩");
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         /* 등록 버튼을 누르면 게시물이 등록이 되며 home으로 리다이렉트 되어야 합니다. */
         /* 작성한 내용과 useNavigate를 이용하여 작성해보세요. */
-       console.log(e.type);
-    }
+        let makeBlog = {
+            title,
+            body,
+            author,
+            likes: 0,
+        };
+        setIsPending(true);
+        fetch("http://localhost:3001/blogs/", {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(makeBlog),
+        }).then(() => {
+            navigate("/", { replace: true });
+            window.location.reload();
+            setIsPending(false);
+        });
+        //console.log(e.type);
+    };
 
     return (
-      <div className="create">
-        <h2>Add a New Blog</h2>
-        <form onSubmit={handleSubmit}>
-            <label>제목</label>
-            <input 
-            type="text" 
-            required 
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="제목을 입력해주세요."
-            />
-            <label>내용</label>
-            <textarea
-            required
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-            placeholder="내용을 입력해주세요."
-            ></textarea>
-            <label>작성자</label>
-            <select
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            >
-            <option value="kimcoding">김코딩</option>
-            <option value="parkhacker">박해커</option>
-            </select>
-            <button>등록</button>
-        </form>
-      </div>
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>제목</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="제목을 입력해주세요."
+                />
+                <label>내용</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                    placeholder="내용을 입력해주세요."
+                ></textarea>
+                <label>작성자</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="kimcoding">김코딩</option>
+                    <option value="parkhacker">박해커</option>
+                </select>
+                <button onClick={handleSubmit}>등록</button>
+            </form>
+            <Footer />
+        </div>
     );
-  }
-  
-  export default CreateBlog; 
+};
+
+export default CreateBlog;
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,18 +1,29 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    // const handleReroad = () => {
+    //     window.location.reload();
+    // };
+
     return (
         <nav className="navbar">
-        <Link to ="/"><h1>The CodeStates Blog💜</h1></Link>
-        <div className="links">
-            <Link to="/create" style={{ 
-                color: 'white', 
-                backgroundColor: '#295CED',
-                borderRadius: '8px' 
-            }}>게시글 작성</Link>
-        </div>
-      </nav>
+            <Link to="/">
+                <h1>The CodeStates Blog💜</h1>
+            </Link>
+            <div className="links">
+                <Link
+                    to="/create"
+                    style={{
+                        color: "white",
+                        backgroundColor: "#295CED",
+                        borderRadius: "8px",
+                    }}
+                >
+                    게시글 작성
+                </Link>
+            </div>
+        </nav>
     );
-}
-  
-export default Navbar; 
+};
+
+export default Navbar;
