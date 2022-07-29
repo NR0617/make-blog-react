39b0352783647959b05b6147bee3d423f33d7447
NR0617 <nara20210617@gmail.com>
2022-07-29 14:57:47 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
 import { useEffect, useState, Suspense, lazy } from "react";
+import useFetch from "./util/useFetch";
 
 /* react.lazy()와 suspense를 사용해 보세요. */
 const Home = lazy(() => import("./Home"));
@@ -18,32 +19,18 @@ function App() {
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
+    const data = useFetch("http://localhost:3001/blogs");
+    //console.log(data);
     /* get 메소드를 통해 데이터를 받아오는 useEffect hook은 컴포넌트 내 여기저기 존재하고 있습니다. */
     /* 해당 hook은 반복이 되는 부분이 있으므로 어떻게 custom hook으로 만들 수 있을 지 고민해봅시다. */
     /* util 폴더 내에 존재하는 useFetch에 여러분의 custom hook을 작성해주세요. */
     useEffect(() => {
         setTimeout(() => {
-            fetch("http://localhost:3001/blogs")
-                .then((res) => {
-                    if (!res.ok) {
-                        throw Error(
-                            "could not fetch the data for that resource"
-                        );
-                    }
-                    return res.json();
-                })
-                .then((data) => {
-                    //console.log(data);
-                    setIsPending(false);
-                    setBlogs(data);
-                    setError(null);
-                })
-                .catch((err) => {
-                    setIsPending(false);
-                    setError(err.message);
-                });
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
         }, 1000);
-    }, []);
+    }, [data]);
 
     return (
         <BrowserRouter>
--- src/util/useFetch.js
+++ src/util/useFetch.js
@@ -2,13 +2,31 @@ import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
     /* useState를 이용하여 data, isPending, error를 정의하세요. */
-    const [data, setData] = useState(null);
+    const [blog, setBlog] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
-
     /* useFetch 안의 중심 로직을 작성해주세요. */
-
-    return; /* return 문을 작성해주세요. */
+    useEffect(() => {
+        //setTimeout(() => {
+        fetch(url)
+            .then((res) => {
+                if (!res.ok) {
+                    throw Error("could not fetch the data for that resource");
+                }
+                return res.json();
+            })
+            .then((resp) => {
+                setIsPending(false);
+                setBlog(resp);
+                setError(null);
+            })
+            .catch((err) => {
+                setIsPending(false);
+                setError(err.message);
+            });
+        // }, 1000);
+    }, [url]);
+    return blog; /* return 문을 작성해주세요. */
 };
 
 export default useFetch;
